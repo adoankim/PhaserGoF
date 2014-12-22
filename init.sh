bde2efb1c044ac2de66c619542e413b641618ec4
adoankim <adoankim@alumnos.uvigo.es>
2014-12-22 10:00:03 +0000

    Add requirejs to Bower dependencies

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@
 bower install 
 mkdir -p public/vendor/js 
 cp bower_components/phaser/build/phaser.{min.js,map} public/vendor/js
+cp bower_components/requirejs/requirejs/require.js public/vendor/js
