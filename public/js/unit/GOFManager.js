fe7ddb9bea68ef382d1e541132856ac020644ef0
adoankim <adoankim@alumnos.uvigo.es>
2014-12-29 23:13:44 +0000

    GoFManager class and test cases added

--- public/js/unit/GOFManager.js
+++ public/js/unit/GOFManager.js
@@ -5,9 +5,152 @@
  * 
  * GOFManager.js
  */
-function GOFManager(DirectedGraph){
+function GOFManager(DirectedGraph, xBound, yBound){
     var GOFManagerClass = function(){
+        this.aliveCells = [];
+        this.currentGeneration = new DirectedGraph();
     }
+    
+    /**
+     * Method that allows the cell insertion to GoF map
+     * @param pos position representation
+     * @return object {x, y}
+     */
+    GOFManagerClass.prototype.addCell = function(pos){
+        var vertex = pos.x + '-' + pos.y;
+        __addCell(this.aliveCells, this.currentGeneration, vertex);
+        return pos;
+    }
+    
+    /**
+     * Method that checks if a given cell is alive or dead
+     * @param pos position representation
+     * @return boolean
+     */
+    GOFManagerClass.prototype.checkCell = function(pos){
+        return this.aliveCells.indexOf(pos.x + '-' + pos.y) != -1;
+    }
+
+    /**
+     * Method that calculate the next generation of living cells
+     * - Firstly it builds the DirectedGraph representation
+     * - Secondly it checks the rules over the current GoF map
+     * - Finally it transforms the alive cells to the original cell representation.
+     * 
+     * @return Array of objects {x,y}
+     */
+    GOFManagerClass.prototype.calculateNextGeneration = function(){
+        var cell;
+        var self = this;
+        this.aliveCells.forEach(function(val){
+            cell = val.split('-').map(function(v){ return parseInt(v);});
+            calculateEdgesAndAddToGraph(self, cell);
+        });
+        
+        applyRulesToCells(this)
+    
+        return this.aliveCells.map(function(val){
+            var pos = val.split('-')
+            return {'x' : pos[0], 'y' : pos[1]};
+        })
+    }
+    
+    // PRIVATE METHODS BELOW
+    
+    /**
+     * Privated method that adds the vertex to the given list and DirectedGraph instance
+     * @param aliveStack list with positions
+     * @param graph DirectedGraph instance
+     * @param vertex position label
+     */
+    function __addCell(aliveStack, graph, vertex){
+        aliveStack.push(vertex);
+        graph.addVertex(vertex);
+    }
+    
+    /**
+     * Privated method that check the alive/dead cell rules to the GoF map
+     * @TODO optimize the ressource consumption of the next generation built process 
+     *       reusing the variables from the current generation.
+     * @param self reference to the current instance
+     */
+    function applyRulesToCells(self){
+        var inbound;
+        var nextGenAlives = [];
+        var nextGenGraph = new DirectedGraph();
+        var vertices = self.currentGeneration.getVertices();
+        vertices.forEach(function(val){
+           inbound = self.currentGeneration.getInboundGrade(val);
+           if(inbound == 3){
+               __addCell(nextGenAlives, nextGenGraph, val);
+           }
+           
+           if(self.aliveCells.indexOf(val) != -1 && inbound == 2){
+               __addCell(nextGenAlives, nextGenGraph, val);
+           }
+        });
+        
+        self.aliveCells = nextGenAlives;
+        self.currentGeneration = nextGenGraph;
+    }
+    
+    /**
+     * Privated method that calculates the neighbours of a given cell 
+     * based on their current position
+     * @param self reference to the current instance
+     * @param cell [x, y]
+     */
+    function calculateEdgesAndAddToGraph(self, cell){
+    
+        if(cell[0] - 1 >= 0){
+            addConnectedNeighbour(self, cell, [cell[0] - 1, cell[1]]);
+            checkYBound(self, cell, -1);
+        }
+        
+        if(cell[0] + 1 < xBound){
+            addConnectedNeighbour(self, cell, [cell[0] + 1, cell[1]]);
+            checkYBound(self, cell, 1);
+        }
+        
+        checkYBound(self, cell, 0);
+    }
+
+    
+    
+    /**
+     * Privated method that control the neighbour connection cells at the bounds 
+     * in Y axis.
+     * @param self reference to the current instance
+     * @param cell [x, y]
+     * @param modifier integer that moves the point in X axis
+     */
+    function checkYBound(self, cell, modifier){
+        if(cell[1] - 1 >= 0){
+            addConnectedNeighbour(self, 
+                cell, 
+                [cell[0] + modifier, cell[1] - 1]);
+        }
+        
+        if(cell[1] + 1 < yBound){
+            addConnectedNeighbour(self, 
+                cell, 
+                [cell[0] + modifier, cell[1] + 1]);
+        }
+    }
+
+    /**
+     * Privated method that connects to neighbours in a dash-separated 
+     * coordinates representation
+     * @param cellA [x, y]
+     * @param cellB [x, y]
+     * @param modifier integer that moves the point in X axis
+     */
+    function addConnectedNeighbour(self, cellA, cellB){
+        self.currentGeneration.addEdge(
+                cellA.join("-"), 
+                cellB.join("-"));
+    }
+    
     
     return new GOFManagerClass();
 }
@@ -20,8 +163,8 @@ function GOFManager(DirectedGraph){
 if(typeof(define) === 'function'){
     define(['unit/DirectedGraph'], function (DirectedGraph) {
         return {
-            getInstance: function () {
-                return new GOFManager(DirectedGraph);
+            getInstance: function (xBound, yBound) {
+                return new GOFManager(DirectedGraph, xBound, yBound);
             }
         };
     });
@@ -29,7 +172,7 @@ if(typeof(define) === 'function'){
 }
 
 //for testing purposes
-module.exports = function(){
+module.exports = function(xBound, yBound){
     var DirectedGraph = require('./DirectedGraph.js');
-    return GOFManager(DirectedGraph);
+    return new GOFManager(DirectedGraph, xBound, yBound);
 }
--- test/testGOFManager.js
+++ test/testGOFManager.js
@@ -11,39 +11,102 @@ var assert = chai.assert,
     expect = chai.expect,
     should = chai.should(); 
 
+var GOFManagerClass = require('../public/js/unit/GOFManager.js');
 
 
 describe('GOFManager', function(){
     
     describe('#addCell(pos)', function(){
+        var GOFMan = new GOFManagerClass(10, 10);
         it('should permit to add a cell in a given position', function(){
-            assert.fail();
+            var newCell = {'x' : 0, 'y' : 0};
+            var addedCell = GOFMan.addCell(newCell);
+            addedCell.should.deep.equal(newCell);
         })
     })
     
     describe('#checkCell(pos)', function(){
+        var GOFMan = new GOFManagerClass(10, 20);
         it('should return if a cell is alive or not', function(){
-            assert.fail();
+            var cell = {'x' : 0, 'y' : 0};
+            GOFMan.addCell(cell);
+
+            var cellIsAlive = GOFMan.checkCell(cell);
+            
+            var cell = {'x' : 0, 'y' : 1};
+            var cellIsDead = GOFMan.checkCell(cell);
+            
+            expect(cellIsAlive).to.be.ok;
+            expect(cellIsDead).to.not.be.ok;
         })
     })
     
     
     describe('#calculateNextGeneration()', function(){
-
         it('Any live cell with fewer than two live neighbours dies', function(){
-            assert.fail();
+            var GOFMan = new GOFManagerClass(3, 3);
+            var cellA = {'x' : 0, 'y' : 0};
+            var cellB = {'x' : 1, 'y' : 1};
+            GOFMan.addCell(cellA);
+            GOFMan.addCell(cellB);
+            GOFMan.calculateNextGeneration();
+            
+            var cellMustBeDead = GOFMan.checkCell(cellA);
+            expect(cellMustBeDead).to.not.be.ok;
+            
+            cellMustBeDead = GOFMan.checkCell(cellB);
+            expect(cellMustBeDead).to.not.be.ok;
         });
     
         it('Any live cell with more than three live neighbours dies', function(){
-            assert.fail()
+            var GOFMan = new GOFManagerClass(3, 3);
+            var cellA = {'x' : 0, 'y' : 0};
+            var cellB = {'x' : 0, 'y' : 2};
+            var cellC = {'x' : 1, 'y' : 1};
+            var cellD = {'x' : 0, 'y' : 2};
+            var cellE = {'x' : 2, 'y' : 2};
+            GOFMan.addCell(cellA);
+            GOFMan.addCell(cellB);
+            GOFMan.addCell(cellC);
+            GOFMan.addCell(cellD);
+            GOFMan.addCell(cellE);
+            GOFMan.calculateNextGeneration();
+            
+            var cellMustBeDead = GOFMan.checkCell(cellC);
+            expect(cellMustBeDead).to.not.be.ok;
+            
         });
         
         it('Any live cell with two or three live neighbours lives', function(){
-            assert.fail()
+            var GOFMan = new GOFManagerClass(3, 3);
+            var cellA = {'x' : 0, 'y' : 0};
+            var cellB = {'x' : 1, 'y' : 0};
+            var cellC = {'x' : 0, 'y' : 1};
+            GOFMan.addCell(cellA);
+            GOFMan.addCell(cellB);
+            GOFMan.addCell(cellC);
+            GOFMan.calculateNextGeneration();
+            
+            var cellMustBeAlive = GOFMan.checkCell(cellA);
+            expect(cellMustBeAlive).to.be.ok;
+            
         });
         
         it('Any dead cell with exactly three live neighbours becomes a live cell', function(){
-            assert.fail()
+            var GOFMan = new GOFManagerClass(3, 3);
+            var cellA = {'x' : 0, 'y' : 0};
+            var cellB = {'x' : 0, 'y' : 2};
+            var cellC = {'x' : 2, 'y' : 0};
+            GOFMan.addCell(cellA);
+            GOFMan.addCell(cellB);
+            GOFMan.addCell(cellC);
+            GOFMan.calculateNextGeneration();
+            
+            
+            var newCell = {'x' : 1, 'y' : 1};
+            var cellMustBeAlive = GOFMan.checkCell(newCell);
+            expect(cellMustBeAlive).to.be.ok;
+            
         });
     })
 
