8361c3798587ac018a7b9458ffc7f1260acb644c
adoankim <adoankim@alumnos.uvigo.es>
2014-12-29 17:22:50 +0000

    Underscore dependency added

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ bower install
 mkdir -p public/vendor/js 
 cp bower_components/phaser/build/phaser.{min.js,map} public/vendor/js
 cp bower_components/requirejs/require.js public/vendor/js
+cp bower_components/underscore/underscore-{min.js,map} public/vendor/js
